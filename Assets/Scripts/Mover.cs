fe6d80e4c8d0746536addbc2012667549c86fa39
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2025-03-30 23:34:12 -0400

    keyboard input/controls

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] float speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +16,14 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 5f);
-        if (transform.position.x > 10f || transform.position.x < -10f || transform.position.y > 10f || transform.position.y < -10f || transform.position.z > 10f || transform.position.z < -10f)
-        {
-            transform.position = new Vector3(0, 0, 0);
-        }
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // if (transform.position.x > 10f || transform.position.x < -10f || transform.position.y > 10f || transform.position.y < -10f || transform.position.z > 10f || transform.position.z < -10f)
+        // {
+        //     transform.position = new Vector3(0, 0, 0);
+        // }
+
+        float xValue = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(xValue, 0, zValue);
     }
 }
